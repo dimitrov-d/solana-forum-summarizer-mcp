af6b002af1b674d17d1c5810fd14616b6e7b6e31
Damjan Dimitrov <damjandimitrov1@gmail.com>
2025-03-13 13:07:17 +0100

    Base files for stagehand actions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello sendai');
+import { Stagehand } from '@browserbasehq/stagehand';
+import { main } from './main.js';
+import StagehandConfig from './stagehand.config.js';
+
+const stagehand = new Stagehand({
+  ...StagehandConfig,
+});
+
+stagehand
+  .init()
+  .then(() =>
+    main({ page: stagehand.page, context: stagehand.context, stagehand }),
+  )
+  .then(() => stagehand.close())
+  .catch((error) => console.error(`Error occurred in main function: ${error}`));
