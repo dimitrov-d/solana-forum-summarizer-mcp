833c12a61c25e6e78407eb6e7895e62f5d0c29f4
Damjan Dimitrov <damjandimitrov1@gmail.com>
2025-03-13 15:16:54 +0100

    Add types, update stagehand config

---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export interface ForumPost {
+  title: string;
+  category: string;
+  age: string;
+  replies: number;
+  views: number;
+  activity: string;
+  author: string;
+  postUrl: string;
+}
